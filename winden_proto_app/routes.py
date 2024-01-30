b913b8b2da0c5043cec0444777a9c393bc7205f8
rappakos <rappakos@gmail.com>
2024-01-30 21:09:51 +0100

    winden proto - start reworking menu

--- winden_proto_app/routes.py
+++ winden_proto_app/routes.py
@@ -1,6 +1,6 @@
 import pathlib
 
-from .views import index, alle_winden, winde,aufbau, piloten,schlepps,schlepp_start, schlepp
+from .views import index,admin
 from .report_views import reports, report, export
 from .admin_views import backups, create_backup, remove_backup
 
@@ -10,26 +10,29 @@ PROJECT_ROOT = pathlib.Path(__file__).parent
 
 def setup_routes(app):
     app.router.add_get('/', index)
-    app.router.add_get('/winden',alle_winden, name='winden')
-    app.router.add_get('/winden/{winde_id}',winde, name='winde')
-    app.router.add_get('/winden/{winde_id}/aufbau', aufbau, name='aufbau') # to load protocol questions
-    app.router.add_post('/winden/{winde_id}/aufbau', aufbau, name='aufbau') # to submit protocol
+    app.router.add_get('/admin', admin)
+
+    ### old UI
+    #app.router.add_get('/winden',alle_winden, name='winden')
+    #app.router.add_get('/winden/{winde_id}',winde, name='winde')
+    #app.router.add_get('/winden/{winde_id}/aufbau', aufbau, name='aufbau') # to load protocol questions
+    #app.router.add_post('/winden/{winde_id}/aufbau', aufbau, name='aufbau') # to submit protocol
     # TODO add abbau
 
-    app.router.add_get('/piloten',piloten, name='piloten')
-    app.router.add_get('/schlepps',schlepps, name='schlepps')
-    app.router.add_post('/schlepps',schlepp, name='schlepp')
-    app.router.add_get('/schlepps/start',schlepp_start, name='schleppstart')
+    #app.router.add_get('/piloten',piloten, name='piloten')
+    #app.router.add_get('/schlepps',schlepps, name='schlepps')
+    #app.router.add_post('/schlepps',schlepp, name='schlepp')
+    #app.router.add_get('/schlepps/start',schlepp_start, name='schleppstart')
     # ...
     
-    app.router.add_get('/reports',reports, name='reports')
-    app.router.add_get('/reports/{report_id}',report, name='report')
-    app.router.add_get('/reports/{report_id}/export',export, name='export')
+    #app.router.add_get('/reports',reports, name='reports')
+    #app.router.add_get('/reports/{report_id}',report, name='report')
+    #app.router.add_get('/reports/{report_id}/export',export, name='export')
     
     # ADMIN?
-    app.router.add_get('/backups',backups, name='backups')
-    app.router.add_get('/backups/create',create_backup, name='create_backup')
-    app.router.add_get('/backups/remove',remove_backup, name='remove_backup')
+    #app.router.add_get('/backups',backups, name='backups')
+    #app.router.add_get('/backups/create',create_backup, name='create_backup')
+    #app.router.add_get('/backups/remove',remove_backup, name='remove_backup')
 
     setup_static_routes(app)
 
