10074acf0176466de14f07b8246eb9fefdff2722
rappakos <rappakos@gmail.com>
2024-02-09 20:25:27 +0100

    Winden proto - reenable winden and pilots menu

--- winden_proto_app/routes.py
+++ winden_proto_app/routes.py
@@ -2,7 +2,8 @@ import pathlib
 
 from .views import index,start_day,cancel_day,activate_winde, \
         aufbau, abbau, \
-        calendar_list,add_calendar_list, admin
+        calendar_list,add_calendar_list, admin, \
+        alle_winden, winde, piloten
 from .report_views import reports, report, export
 from .admin_views import backups, create_backup, remove_backup
 
@@ -22,12 +23,12 @@ def setup_routes(app):
     app.router.add_post('/winden/{winde_id}/abbau', abbau, name='abbau') # TODO implement
     app.router.add_get('/admin', admin)
 
-    ### old UI
-    #app.router.add_get('/winden',alle_winden, name='winden')
-    #app.router.add_get('/winden/{winde_id}',winde, name='winde')
-    # TODO add abbau
+    
+    app.router.add_get('/winden',alle_winden, name='winden')
+    app.router.add_get('/winden/{winde_id}',winde, name='winde')
 
-    #app.router.add_get('/piloten',piloten, name='piloten')
+
+    app.router.add_get('/piloten',piloten, name='piloten')
     #app.router.add_get('/schlepps',schlepps, name='schlepps')
     #app.router.add_post('/schlepps',schlepp, name='schlepp')
     #app.router.add_get('/schlepps/start',schlepp_start, name='schleppstart')
